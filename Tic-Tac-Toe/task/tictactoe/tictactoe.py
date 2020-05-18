592042212f4be2afc31d48c043e4f6a6fb4149ba
nlg734 <npsm111@gmail.com>
2020-05-18 07:47:04 -0500

    stage 2 complete

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,4 +1,9 @@
 # write your code here
-print("X O X")
-print("O X O")
-print("X X O")
+
+
+field = input("Enter cells:")
+print("---------")
+print("|", field[0], field[1], field[2], "|")
+print("|", field[3], field[4], field[5], "|")
+print("|", field[6], field[7], field[8], "|")
+print("---------")
